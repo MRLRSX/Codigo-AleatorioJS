c2e865f51ff1d9da981144fec0ed5e3fec75ea6a
MRLRSX <lucas.rocha-dev@outlook.com>
2023-01-20 22:09:29 -0300

    tipagem fraca ou tipagem dinamica?

---
+++ typeBooleanDois.js
@@ -0,0 +1,21 @@
+const usuarioLogado = true;
+const contaPaga = false;
+
+// truthy ou falsy
+// 0 e 1
+//0 => false
+// 1 => true
+
+console.log(0 == false);
+console.log("" == false);
+
+// null 
+//undefined
+
+let minhaVar;
+var varNull = null;
+let texto = "Lucas Rocha";
+
+console.log(typeof minhaVar);
+console.log(typeof varNull); // null e objeto 
+console.log(typeof texto);
