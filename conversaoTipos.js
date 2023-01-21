84da9057a5db4bb65c5f4c8bbf0865fb70d85051
MRLRSX <lucas.rocha-dev@outlook.com>
2023-01-21 01:46:56 -0300

    tipagem fraca ou tipagem dinamica?

---
+++ conversaoTipos.js
@@ -0,0 +1,11 @@
+// tipo de dados 
+// conversão implícita 
+const numero = 456;
+const numeroString = "456";
+
+console.log(numero === numeroString);
+console.log(numero == numeroString);
+
+
+// conversâo explícita
+console.log(numero + Number.parseInt(numeroString));
