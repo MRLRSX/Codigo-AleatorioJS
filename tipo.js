ddfbc58686f44b96af780371374d9412d8089505
MRLRSX <lucas.rocha-dev@outlook.com>
2023-01-20 21:12:57 -0300

    Tipos Basicos

---
+++ tipo.js
@@ -0,0 +1,16 @@
+const meuNumero = 3;
+const segundoNumero = 10;
+const operacao = meuNumero + segundoNumero;
+
+console.log(operacao * 3);
+
+const operacaoDois = (operacao * 2) / 3;
+console.log(operacaoDois);
+
+const numeroPontoFlutuante = 3.3;
+const numeroPontoFlutuanteDois = .3;
+
+
+// Nan igual no Java (não e numero)
+
+console.log(numeroPontoFlutuante, numeroPontoFlutuanteDois);
