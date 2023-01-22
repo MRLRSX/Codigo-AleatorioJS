0019170a935c05c44ff1a65e9049c7731eee4496
MRLRSX <lucas.rocha-dev@outlook.com>
2023-01-21 23:12:59 -0300

    tipagem fraca ou tipagem dinamica?

--- lista-chamada.js
+++ lista-chamada.js
@@ -1,3 +1,4 @@
 const chamada = ["João", "Ana", "Caio", "Lara", "Marjorie", "Leo"];
-chamada.splice(chamada.findIndex(x => x === 'Ana'), chamada.findIndex(x => x === 'Caio'));
+chamada.splice(chamada.findIndex(x => x === 'Ana'), chamada.findIndex(x => x === 'Caio'), 'Lucas Rocha'); // testando duas funções
+
 console.log(chamada);
