0f55c8f21a62d72ac641c0d32b0742d22a91625a
MRLRSX <lucas.rocha-dev@outlook.com>
2023-01-21 22:04:11 -0300

    tipagem fraca ou tipagem dinamica?

--- funcao.js
+++ funcao.js
@@ -1,10 +1,11 @@
 let numeroUm = 5;
 let numeroDois = 7;
 const multiplicado = (x, y) => x * y;
-
+const apresenta = name => `My name is ${name}`;
 console.log(area(numeroUm, numeroDois));
 imprimirConsole(soma(numeroUm, numeroDois)); // HUUMMMM
 console.log(multiplicado(numeroUm, numeroDois));
+console.log(apresenta("Lucas Rocha dos Santos"))
 
 
 function area(num1, num2){
