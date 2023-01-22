978e78fa06903f3384d47d2eb23767b1fc9c0314
MRLRSX <lucas.rocha-dev@outlook.com>
2023-01-22 11:44:23 -0300

    Objetos ou JSON

---
+++ aulas-objetos/tiposObjetosDois.js
@@ -0,0 +1,13 @@
+const estudante = {
+    nome: "Pablo Henrique Lima",
+    matricula: 201542090,
+    curso: "Engenharia eletrônica",
+    materias: {
+      materia1: "Cálculo 1",
+      materia2: "Álgebra Linear",
+      materia3: "Práticas de Laboratório",
+      materia4: "Metodologia",
+    },
+  };
+
+  console.log(estudante);
