3b19bc9631161e37ffe01a7c951bb464f1347c94
MRLRSX <lucas.rocha-dev@outlook.com>
2023-01-22 04:19:31 -0300

    tipagem fraca ou tipagem dinamica?

---
+++ forNumeros.js
@@ -0,0 +1,7 @@
+const numeros = [100, 200, 300, 400, 500, 600];
+numeros.forEach(x => console.log(x));
+
+for(let x = 0; x < numeros.length; x++) {
+    console.log(numeros[x]);
+}
+
--- pesquisa.js
+++ pesquisa.js
@@ -12,5 +12,12 @@ const indice = listaDeAlunosEMedias[0].indexOf(nome);
 const media = listaDeAlunosEMedias[1][indice];
 console.log(`Id:${indice} Nome: ${nome} Média ${media}`);
 
+const exemplo = "Ana";
+const textoGrande = listaDeAlunosEMedias[0].includes(exemplo) == true 
+? `Id ${listaDeAlunosEMedias[0].indexOf(exemplo)} Nome ${exemplo} ${listaDeAlunosEMedias[1][listaDeAlunosEMedias[0].indexOf(exemplo)]}` 
+: `Não Cadastrado ${exemplo}`; 
+
+console.log(textoGrande);
+
 
 
